d47d4c2b7da4c47f9720ace365c6a4fcea2c8fea
Austin Hamidi <115668777+ahamidiChapman@users.noreply.github.com>
2024-02-14 21:54:18 -0800

    Add files via upload

---
+++ Lecture-2-HW-Fixed.ts
@@ -0,0 +1,18 @@
+import { SecureString } from 'secure-string-library';
+
+interface User {
+    username: string;
+    password: SecureString;
+}
+
+interface LoginCredentials {
+    username: string;
+    password: string;
+}
+
+interface HashedPassword {
+    algorithm: string; // Details specific to the chosen hashing algorithm
+    hash: string; // Hashed password value
+}
+
+export { User, LoginCredentials, HashedPassword };
